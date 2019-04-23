fdb833b49aef7e66bbbab9f7cb7500ce7182414d
Tom Wayson <tom@tomwayson.com>
2019-04-23 16:33:06 -0700

    adds ember-auto-import, @esri/hub-events, @esri/hub-common as addon deps
    As per: https://github.com/ef4/ember-auto-import#usage-from-addons

--- packages/example-addon/addon/components/hello-world/component.js
+++ packages/example-addon/addon/components/hello-world/component.js
@@ -1,6 +1,21 @@
 import Component from '@ember/component';
 import layout from './template';
+// import from Hub.js packages that are direct dependencies of this addon
+import { getHubApiUrl } from '@esri/hub-common';
+import { getEventQueryFromType } from '@esri/hub-events';
 
 export default Component.extend({
-  layout
+  layout,
+  init () {
+    this._super(arguments);
+    // demonstrate that we can use the fns from the Hub.js packages
+    const ro = {
+      portal: 'https://custom.maps.arcgis.com/sharing/rest'
+    };
+    this.set('hubApiUrl', getHubApiUrl(ro));
+    const options = getEventQueryFromType('upcoming', {});
+    /* eslint-disable no-console */
+    console.log({ options });
+    /* eslint-enable no-console */
+  }
 });
