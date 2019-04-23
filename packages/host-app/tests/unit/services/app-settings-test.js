481320e6a1d44ebd4d60e924931b9c6fc79e81a7
Tom Wayson <tom@tomwayson.com>
2019-04-23 16:53:37 -0700

    adds ember-auto-import & @esri/hub-common as deps to host-app
    this works by happy accident of ember's module loader, but we should:
    - make @esri/hub-common a peerDependency (instead of a dependency) of the addon
    - use addPackageToApp() in the addon's blueprint to ensure it gets installed

--- packages/host-app/tests/unit/services/app-settings-test.js
+++ packages/host-app/tests/unit/services/app-settings-test.js
@@ -4,9 +4,13 @@ import { setupTest } from 'ember-qunit';
 module('Unit | Service | app-settings', function(hooks) {
   setupTest(hooks);
 
-  // Replace this with your real tests.
-  test('it exists', function(assert) {
+  test('clone', function(assert) {
     let service = this.owner.lookup('service:app-settings');
-    assert.ok(service);
+    // just verify that we can call clone,
+    // really to test this we'd stub cloneObject() and verify that it was called
+    const obj = { string: 'hi' };
+    const clone  = service.clone(obj);
+    assert.deepEqual(obj, clone, 'clone is deep equal');
+    assert.notEqual(obj, clone, 'clone is not the same object');
   });
 });
